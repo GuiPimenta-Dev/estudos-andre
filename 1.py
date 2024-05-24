ce42aef4bf57c0a4d8e781908d6074923944f968
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-05-24 17:35:17 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,22 @@
 # Exercicio 1: Calculadora Simples
-# Instrucao: Escreva um programa que peça ao usuário dois números e exiba a soma, subtração, multiplicação e divisão deles.
+# Instrucao: Escreva um programa que exiba a soma, subtração, multiplicação e divisão deles.
 # Tópicos para estudar:
 # - Entrada de dados (input)
 # - Conversão de tipos (int, float)
 # - Operações aritméticas básicas (+, -, *, /)
 # - Impressão de dados (print)
-
-# Código aqui
-
+'''
+numero_1 =int( input("insert number one: "))
+numero_2 =int( input("insert number two: "))
+soma  =numero_1+numero_2
+divisão = numero_1/numero_2
+subtração = numero_1-numero_2
+multiplicação =numero_1*numero_2
+print(soma)
+print(subtração)
+print(divisão)
+print(multiplicação)
+'''
 # Exercicio 2: Média de três números
 # Instrucao: Escreva um programa que peça ao usuário três números e calcule a média deles.
 # Tópicos para estudar:
@@ -15,9 +24,17 @@
 # - Conversão de tipos (int, float)
 # - Operações aritméticas básicas (+, /)
 # - Impressão de dados (print)
+'''
+numero_4 =int(input("insert number one"))
+numero_5 =int(input("insert number two"))
+numero_6 =int(input("insert number tres"))
 
-# Código aqui
+numerador =numero_4+numero_5+numero_6
+denominador =3
+media =numerador/denominador
 
+print(media)
+'''
 # Exercicio 3: Conversão de Temperatura
 # Instrucao: Escreva um programa que peça ao usuário a temperatura em graus Celsius e a converta para Fahrenheit.
 # Fórmula: F = C * 9/5 + 32
@@ -26,5 +43,8 @@
 # - Conversão de tipos (int, float)
 # - Operações aritméticas básicas (*, +, /)
 # - Impressão de dados (print)
-
-# Código aqui
+'''
+C =int(input("insira o valor em celsius"))
+conversao =C*(9/5)+32
+print(conversao)
+'''
