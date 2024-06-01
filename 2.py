303d165b3b45545eb75a6c85bb9d4c461b9add4d
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-06-01 14:28:15 -0300

    fixing

--- 2.py
+++ 2.py
@@ -1,8 +1,21 @@
-# Exercicio 1: Verificação de Palíndromo
-# Instrucao: Escreva um programa que peça ao usuário uma palavra e verifique se é um palíndromo.
+# Tópicos para estudar:
+
+
+# Booleanos (https://panda.ime.usp.br/aulasPython/static/aulasPython/aula05.html)
+# Condicionais (https://www.hashtagtreinamentos.com/estruturas-condicionais-no-python?gad_source=1&gclid=Cj0KCQjw3tCyBhDBARIsAEY0XNkhqehM5sFoA8OpJgtRrGEYQW_IJIS-UQnRKbA0nyvweXsbUhTBxVcaAhpZEALw_wcB)
+"""
+== -> Igualdade
+!= -> Diferença
+> -> Maior que
+< -> Menor que
+"""
+# Lista (https://www.hashtagtreinamentos.com/listas-no-python?gad_source=1&gclid=Cj0KCQjw3tCyBhDBARIsAEY0XNn4i3dikKuwPB2R1c-M4Jk3XaEzlxOoX4ei2hmd6zKAmdylzQP-XlAaAhLREALw_wcB)
+
+# Exercicio 1: Comparação de Strings
+# Instrucao: Escreva um programa que peça ao usuário duas strings e verifique se são iguais, ignorando maiúsculas e minúsculas.
 # Tópicos para estudar:
 # - Entrada de dados (input)
-# - Manipulação de strings (slicing, lower)
+# - Manipulação de strings (lower)
 # - Estruturas condicionais (if, else)
 # - Impressão de dados (print)
 
@@ -19,12 +32,7 @@
 
 # Código aqui
 
-# Exercicio 3: Comparação de Strings
-# Instrucao: Escreva um programa que peça ao usuário duas strings e verifique se são iguais, ignorando maiúsculas e minúsculas.
-# Tópicos para estudar:
-# - Entrada de dados (input)
-# - Manipulação de strings (lower)
-# - Estruturas condicionais (if, else)
-# - Impressão de dados (print)
 
-# Código aqui
+
+
+ 
