7389ed14e7119fde2bf96c963c66de377e81f8de
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-06-02 13:18:52 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -41,7 +41,13 @@ else:
 # - Operadores aritméticos (%)
 # - Estruturas condicionais (if, else)
 # - Impressão de dados (print)
-
+'''
+numero = int(input("insira um numero"))
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
+'''
 
 # Exercício 3: Verificação de Idade
 # Instrucao: Escreva um programa que peça ao usuário sua idade e verifique se ele é maior de idade (18 anos ou mais).
@@ -50,16 +56,26 @@ else:
 # - Conversão de tipos (int)
 # - Estruturas condicionais (if, else)
 # - Impressão de dados (print)
-
-
-
+'''
+idade = int(input("idade do usuario"))
+if idade >=18 :
+  print("você é maior de idade")
+else:
+  print("você é menor de idade")
+'''
 # Exercício 4: Devolva a priemira e ultima palavra de uma frase
 # Instrucao: Escreva um programa que peça ao usuário uma frase e devolva na tela a primeira palavra e a ultima dessa frase.
 # - Entrada de dados (input)
 # - Lista 
+'''
+frase = input("insira a frase")
 
+palavra =frase.split()
+primeira_frase=palavra[0]
+ultima_palavra=palavra[-1]
 
-
-
+print("Primeira palavra:",primeira_frase)
+print("Ultima palavra", ultima_palavra)
+'''
 
  
