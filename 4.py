4293b9b208fb64f261345c7a20070e49082bfe7a
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-07-01 22:59:38 -0300

    primeiro exercicio de classe Rengar

---
+++ 4.py
@@ -0,0 +1,36 @@
+#### CLASSES ####
+
+# class Carro():
+  
+#   velocidade = 0
+  
+#   def acelerar(self, outra_velocidade):
+#     self.velocidade = self.velocidade + outra_velocidade
+
+#   def freiar(self, outra_velocidade):
+#     self.velocidade = self.velocidade - outra_velocidade
+
+# # Instanciando um objeto a partir da classe Carro
+# corvete = Carro()
+# print(corvete.velocidade)
+
+# corvete.acelerar(10)
+# print(corvete.velocidade)
+
+# corvete.acelerar(50)
+# print(corvete.velocidade)
+
+# corvete.acelerar(40)
+# print(corvete.velocidade)
+
+# corvete.freiar(20)
+# print(corvete.velocidade)
+
+# Exercicio 1: Rengar
+# Instrucao: Escreva uma classe chamada Rengar que tenha 4 métodos: Q, W, E, R.
+# O Rengar deve ter uma propriedade Energia, que comeca em 500.
+# O método Q deve imprimir "Rengar usou Q: Savagery, Este metodo deve diminuir a energia em 30"
+# O método W deve imprimir "Rengar usou W: Battle Roar, Este metodo deve diminuir a energia em 40"
+# O método E deve imprimir "Rengar usou E: Bola Bola, Este metodo deve diminuir a energia em 50"
+# O método R deve imprimir "Rengar usou R: Thrill of the Hunt, Este metodo deve diminuir a energia em 100"
+# Caso a energia seja menor que o custo de um dos métodos, o método deve imprimir "Rengar não tem energia suficiente para usar este método"
